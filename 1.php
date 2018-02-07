ac405338112e89f7e21178d2342adee3e2481818
kimdaweon <36214548+kimdaweon@users.noreply.github.com>
2018-02-07 10:22:02 +0900

    20180207
    20180207

---
+++ 1.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<title>부기의연습장</title>
+	<meta charset="UTF-8">
+</head>
+<body>
+	<h1><a href="new.php">DOTEON</a></h1>
+	<ol>
+		<li><a href="1.php">부기</a></li>
+		<li><a href="2.php">자라</a></li>
+		<li><a href="3.php">도트온</a></li>
+	</ol>
+	<h2>부기</h2>
+	<img src="buuk.gif" width="500px">
+	<p style="margin-top:45px;">
+		<a href="https://www.instagram.com/dote___on/" target="_blank" title="doteon">Dote_On</a>
+	커몽커몽
+	</p>
+ </body>
+</html>
